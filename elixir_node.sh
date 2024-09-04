eb23e6c67c78ad41161de3f651547f50d8a5dbad
Alexander Golenev <algolenev@yandex.ru>
2024-09-04 18:33:19 +0300

    v 1.1

--- elixir_node.sh
+++ elixir_node.sh
@@ -70,10 +70,10 @@ while true; do
                 sleep 1
 
                 if sudo apt install apt-transport-https ca-certificates curl software-properties-common -y &&
-                curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &&
-                sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" &&
-                sudo apt-get update &&
-                sudo apt-get install --only-upgrade docker-ce docker-ce-cli containerd.io docker-compose-plugin -y; then
+                    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &&
+                    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" &&
+                    sudo apt-get update &&
+                    sudo apt-get install --only-upgrade docker-ce docker-ce-cli containerd.io docker-compose-plugin -y; then
                     sleep 1
                     echo -e "Обновление Docker (Docker update): \e[32mУспешно (Success)\e[0m"
                     echo ""
